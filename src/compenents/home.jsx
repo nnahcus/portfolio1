e47268eaf774f8a2bb1acee0547c4929d692b4a6
nnahcus <suchanyac23@gmail.com>
2025-10-11 00:20:40 +0700

    navbar done and some hero

--- src/compenents/home.jsx
+++ src/compenents/home.jsx
@@ -31,23 +31,26 @@ function Home() {
     <Background>
       <Navbar />
       {/* intro */}
-      <section className="pt-24 pb-28 px-5 sm:px-6 md:px-12 lg:px-16 xl:px-20">
-        <div className="container mx-auto ">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-clashdisplay font-medium ">Hello, Sawasdee ka</h1>
-          <h3 className="text-2xl sm:text-3xl md:text-4xl py-4  font-clashdisplay">It's me <span className="text-white">Suchanya Chunlabut</span></h3>
-          <p className="text-md sm:text-lg py-1 font-poppins">I passionate about coding and programming.</p>
-          <p className="text-md sm:text-lg py-1 font-poppins">I enjoy building web applications, and solving challenging problems.</p>
-          <div className="flex gap-4 py-4">          
+      <section id="hero" className="pt-24 md:pt-32 px-4 sm:px-6 md:px-12 lg:px-16 xl:px-20 min-h-screen ">
+        <div className="container mx-auto">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-clashdisplay font-medium max-w-4xl">Hello, Sawasdee ka</h1>
+          <h3 className="text-2xl sm:text-3xl md:text-4xl py-4 font-clashdisplay">It's me <span className="text-white">Suchanya Chunlabut</span></h3>
+          <p className="text-md sm:text-lg max-w-2xl py-1 font-poppins">I passionate about coding and programming.</p>
+          <p className="text-md sm:text-lg max-w-2xl py-1 font-poppins">I enjoy building web applications, and solving challenging problems.</p>
+          <div className="flex gap-4 py-8">          
           <Link to="https://github.com/">
-          <button type="button" className="hover:bg-zinc-100 font-chillax tracking-wide bg-white px-6 py-2.5 text-lg rounded-xl inline-flex items-center text-center gap-2 cursor-pointer outline outline-offset-0 outline-solid outline-black">Contact me </button>
+          <button type="button" className="hover:bg-zinc-100  font-chillax tracking-wide bg-white px-6 py-2.5 text-md sm:text-lg md:text-xl rounded-xl inline-flex items-center text-center gap-2 cursor-pointer outline outline-offset-0 outline-solid outline-black">Contact me </button>
+          </Link>
+          <Link to="https://github.com/">
+          <button type="button" className="hover:bg-zinc-100 font-chillax tracking-wide bg-white px-6 py-2.5 text-md sm:text-lg md:text-xl rounded-xl inline-flex items-center text-center gap-2 cursor-pointer outline outline-offset-0 outline-solid outline-black">View My Work </button>
           </Link>
           </div>
         </div>
       </section>
       {/* หน้า about */}
-      <section id="about" className="pt-24 pb-28 py-20 px-5 sm:px-6 md:px-12 lg:px-16 xl:px-20">
+      <section id="about" className="pt-24 md:pt-40 px-5 sm:px-6 md:px-12 lg:px-16 xl:px-20 min-h-screen">
         <div className="container mx-auto">          
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-clashdisplay font-medium ">Hello, Sawasdee ka</h1>
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-clashdisplay font-medium ">About Me</h1>
           <h3 className="text-2xl sm:text-3xl md:text-4xl py-4  font-clashdisplay">It's me <span className="text-white">Suchanya Chunlabut</span></h3>
           <p className="text-md sm:text-lg py-1 font-poppins">I passionate about coding and programming.</p>
           <p className="text-md sm:text-lg py-1 font-poppins">I enjoy building web applications, and solving challenging problems.</p>
