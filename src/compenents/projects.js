fa3b628a3fffe01adefdea4cd8158bd9d5b71276
nnahcus <suchanyac23@gmail.com>
2026-02-01 00:06:59 +0700

    update picture folder from src to public

--- src/compenents/projects.js
+++ src/compenents/projects.js
@@ -4,7 +4,7 @@ const projects = [
         description: "A full-stack media tracking website you can track movies, tv shows, series you have watched and want to watch. Built with Next.js, Tailwind CSS, Supabase, The Movie Database (TMDb) API and Jikan API.",
         link: "https://bingelogg.vercel.app/",
         id: 1,
-        image: "./src/pic/bingelogg.vercel.app_dashboard (1).png",
+        image: "/pic/bingelogg.vercel.app_dashboard (1).png",
         github: "https://github.com/nnahcus/bingelogg"
     },
     {
@@ -12,7 +12,7 @@ const projects = [
         description: "My personal portfolio website to showcase my skills, projects. Built with React, Tailwind CSS, and Vite.",
         link: "https://suchanyaportfolio.vercel.app/",
         id: 2,
-        image: "./src/pic/localhost_5173_.png",
+        image: "/pic/localhost_5173_.png",
         github: "https://github.com/nnahcus/portfolio1"
     }
 ];
